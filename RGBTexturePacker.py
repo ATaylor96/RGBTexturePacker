b265eb57e427ee06cc57d14de2d87cbc8ca6d152
AdamTaylor <adam.f.taylor@hotmail.com>
2022-10-24 19:58:02 +0100

    GUI mock-up and initial GUI loading

--- RGBTexturePacker.py
+++ RGBTexturePacker.py
@@ -1,17 +1,32 @@
-from PIL import Image
+import sys
+from PyQt5 import QtWidgets, uic
 
-r = Image.open("Textures\\roughness.tga").convert('RGB')
-g = Image.open("Textures\\metallic.tga").convert('RGB')
-b = Image.open("Textures\\ao.tga").convert('RGB')
-a = Image.open("Textures\\opacity.tga").convert('RGB')
-output_path = "Textures\\result.tga"
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('F:/Projects/Repo/Python/RGBTexturePacker/main_window.ui', self)
+        self.show()
 
-print(r.size)
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
 
-if a != "":
-    result = Image.merge("RGBA", [r.convert("L"), g.convert("L"), b.convert("L"), a.convert("L")])
 
-else:
-    result = Image.merge("RGB", [r.convert("L"), g.convert("L"), b.convert("L")])
 
-result.save(output_path)
+# from PIL import Image
+#
+# r = Image.open("Textures\\roughness.tga").convert('RGB')
+# g = Image.open("Textures\\metallic.tga").convert('RGB')
+# b = Image.open("Textures\\ao.tga").convert('RGB')
+# a = Image.open("Textures\\opacity.tga").convert('RGB')
+# output_path = "Textures\\result.tga"
+#
+# print(r.size)
+#
+# if a != "":
+#     result = Image.merge("RGBA", [r.convert("L"), g.convert("L"), b.convert("L"), a.convert("L")])
+#
+# else:
+#     result = Image.merge("RGB", [r.convert("L"), g.convert("L"), b.convert("L")])
+#
+# result.save(output_path)
